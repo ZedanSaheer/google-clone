89c247b0a597a0d92e040889ae703dbecd888e9d
Zedan Saheer <zed.saheer5@gmail.com>
2021-09-18 18:29:51 +0530

    tailwind is amazing. next.js seems fun. this commit is very unproffesional yeah!

---
+++ components/Avatar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Avatar = ({ url }) => {
+    return (<img loading="lazy" className="h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110" src={url} alt="profile picture"/>
+    )
+}
+
+export default Avatar
---
+++ components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { GlobeIcon } from '@heroicons/react/solid'
+
+const Footer = () => {
+    return (
+        <div>
+            <p>Saudi Arabia</p>
+        </div>
+    )
+}
+
+export default Footer
